5861e2b9042a6193fcc4cb9a7ceeac2d54a42c30
BuzzLauYear <lauracastrobecerra@gmail.com>
2023-07-31 20:56:41 +0200

    poolDB

---
+++ db/pool.js
@@ -0,0 +1,27 @@
+const mysql = require("mysql2/promise")
+
+require("dotenv").config()
+
+const {DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE} = process.env
+
+let pool
+
+const getPool = async ()=>{
+    if(!pool) {
+        pool = mysql.createPool({
+            connectionLimit: 10,
+            host: DB_HOST,
+            user: DB_USER,
+            password: DB_PASSWORD,
+            database: DB_DATABASE,
+            timezone: "local"
+
+
+
+        }
+
+        );
+    }
+    return await pool.getConnection()
+}
+
