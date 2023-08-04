5eb3a5015e5ee4da0987fcfeec83e2a4960b4dca
Raquel <rmontenemoovecars@gmail.com>
2023-08-04 23:20:32 +0200

    Modificación perfil

---
+++ servicios/avatar.js
@@ -0,0 +1,37 @@
+const crypto = require("node:crypto");
+const fs = require("fs/promises");
+const path = require("path");
+const sharp = require("sharp");
+const { RUTA_AVATAR } = process.env;
+const generarError = require("../helpers/generarError");
+
+async function guardarAvatar(avatar) {
+  try {
+    const rutaAvatar = path.resolve(__dirname, "../", RUTA_AVATAR);
+
+    //Crear ruta para el avatar
+    try {
+      await fs.access(rutaAvatar);
+    } catch {
+      await fs.mkdir(rutaAvatar);
+    }
+
+    const nombreAvatar = (archivo) => {
+      const sharpImg = sharp(archivo.data);
+      sharpImg.resize(100);
+      const nombreAletorio = crypto.randomUUID();
+      const extension = path.extname(archivo.name);
+      const nombreFoto = `${nombreAletorio}${extension}`;
+      const rutaCompleta = path.join(rutaAvatar, nombreFoto);
+      sharpImg.toFile(rutaCompleta);
+      return nombreFoto;
+    };
+
+    const nombre = nombreAvatar(avatar);
+    return nombre;
+  } catch (error) {
+    generarError("No se ha podido guardar el avatar");
+  }
+}
+
+module.exports = guardarAvatar;
