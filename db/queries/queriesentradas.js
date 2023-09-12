1bcb262ef6e926206444385255546aff9bc15a1b
David Grilo <davidgrilom@gmail.com>
2023-09-12 13:27:26 +0200

    cambios votos

--- db/queries/queriesentradas.js
+++ db/queries/queriesentradas.js
@@ -9,7 +9,7 @@ async function getAll(votos) {
     connection = await getPool();
     if (votos) {
       [entradas] = await connection.query(
-        "SELECT e.*, COUNT(v.id), AS total_votos, COUNT(c.id) AS total_comments, u.name, u.avatar FROM entradas e LEFT JOIN votos v ON e.id = v.entrada_id LEFT JOIN usuarios u ON e.user_id = u.id LEFT JOIN comentarios c ON e.id = c.entrada_id GROUP BY e.id ORDER BY total_votos DESC LIMIT 3"
+        "SELECT e.*, COUNT(v.id) AS total_votos, COUNT(c.id) AS total_comments, u.name, u.avatar FROM entradas e LEFT JOIN votos v ON e.id = v.entrada_id LEFT JOIN usuarios u ON e.user_id = u.id LEFT JOIN comentarios c ON e.id = c.entrada_id GROUP BY e.id ORDER BY total_votos DESC LIMIT 3"
       );
     } else {
       [entradas] = await connection.query(
@@ -34,7 +34,7 @@ async function getId(id) {
     connection = await getPool();
 
     const [entradas] = await connection.query(
-      "SELECT e.*, u.name, u.avatar, COUNT(v.id) AS total_votos FROM entradas e LEFT JOIN votos v ON e.id = v.entrada_id LEFT JOIN usuarios u ON e.user_id = u.id WHERE e.id = ? GROUP BY e.id",
+      "SELECT e.*, u.name, u.avatar, COUNT(v.id) AS total_votos, COUNT(c.id) AS total_comments FROM entradas e LEFT JOIN votos v ON e.id = v.entrada_id LEFT JOIN usuarios u ON e.user_id = u.id LEFT JOIN comentarios c ON e.id = c.entrada_id WHERE e.id = ? GROUP BY e.id",
       [id]
     );
 
