37efeeb5b0521976bcd3d8dc4ead8eeb09be0ff3
Raquel <rmontenemoovecars@gmail.com>
2023-09-21 12:13:55 +0200

    arreglo mensajes de joi

--- schemas/esquemasentradas.js
+++ schemas/esquemasentradas.js
@@ -1,10 +1,34 @@
 const joi = require("joi");
 
 const esquemasEntradas = joi.object({
-  titulo: joi.string().min(5).max(50).required(),
-  categoria: joi.string().min(2).max(50).required(),
-  lugar: joi.string().min(2).max(100).required(),
-  texto: joi.string().min(15).max(65000).required(),
+  titulo: joi.string().min(6).max(150).required().messages({
+    "string.base": "Título debe ser de tipo texto",
+    "string.min": "Título debe tener mínimo 6 caracteres",
+    "string.max": "Título debe tener máximo 150 caracteres",
+    "any.required": "Título es obligatorio",
+    "string.empty": "Título no puede estar vacío",
+  }),
+  categoria: joi.string().min(2).max(50).required().messages({
+    "string.base": "Categoría debe ser de tipo texto",
+    "string.min": "Categoría debe tener mínimo 2 caracteres",
+    "string.max": "Categoría debe tener máximo 50 caracteres",
+    "any.required": "Categoría es obligatorio",
+    "string.empty": "Categoría no puede estar vacío",
+  }),
+  lugar: joi.string().min(2).max(150).required().messages({
+    "string.base": "Lugar debe ser de tipo texto",
+    "string.min": "Lugar debe tener mínimo 2 caracteres",
+    "string.max": "Lugar debe tener máximo 150 caracteres",
+    "any.required": "Lugar es obligatorio",
+    "string.empty": "Lugar no puede estar vacío",
+  }),
+  texto: joi.string().min(15).max(65000).required().messages({
+    "string.base": "Descripción debe ser de tipo texto",
+    "string.min": "Descripción debe tener mínimo 15 caracteres",
+    "string.max": "Descripción debe tener máximo 65000 caracteres",
+    "any.required": "Descripción es obligatorio",
+    "string.empty": "Descripción no puede estar vacío",
+  }),
 });
 
 module.exports = esquemasEntradas;
--- schemas/esquemasusuarios.js
+++ schemas/esquemasusuarios.js
@@ -1,22 +1,67 @@
 const joi = require("joi");
 
 // Esquema joi validar registro de usuario
-const esquemaRegistro = joi.object({
-  name: joi.string().min(2).max(50).required(),
-  email: joi.string().email().min(5).max(80).required(),
-  password: joi.string().min(6).max(100).required(),
-  avatar: joi.allow("")
-}).options({ abortEarly: false });
+const esquemaRegistro = joi
+  .object({
+    name: joi.string().min(2).max(50).required().messages({
+      "string.base": "Nombre debe ser de tipo texto",
+      "string.min": "Nombre debe tener mínimo 2 caracteres",
+      "string.max": "Nombre debe tener máximo 50 caracteres",
+      "string.empty": "Nombre no puede estar vacío",
+      "any.required": "Nombre es obligatorio",
+    }),
+    email: joi.string().email().min(5).max(80).required().messages({
+      "string.base": "Email debe ser de tipo email",
+      "string.min": "Email debe tener mínimo 5 caracteres",
+      "string.max": "Email debe tener máximo 80 caracteres",
+      "string.empty": "Email no puede estar vacío",
+      "any.required": "Email es obligatorio",
+    }),
+    password: joi.string().min(6).max(100).required().messages({
+      "string.base": "Password debe ser de tipo texto",
+      "string.min": "Password debe tener mínimo 6 caracteres",
+      "string.max": "Password debe tener máximo 100 caracteres",
+      "any.required": "Password es obligatorio",
+      "string.empty": "Password no puede estar vacío",
+    }),
+    avatar: joi.allow(""),
+  })
+  .options({ abortEarly: false });
 
-const esquemaLogin = joi.object({
-  email: joi.required(),
-  password: joi.required(),
-}).options({ abortEarly: false });
+const esquemaLogin = joi
+  .object({
+    email: joi.required().messages({
+      "string.base": "Email debe ser de tipo email",
+      "string.min": "Email debe tener mínimo 5 caracteres",
+      "string.max": "Email debe tener máximo 80 caracteres",
+      "string.empty": "Email no puede estar vacío",
+      "any.required": "Email es obligatorio",
+    }),
+    password: joi.required().messages({
+      "string.base": "Password debe ser de tipo texto",
+      "string.min": "Password debe tener mínimo 6 caracteres",
+      "string.max": "Password debe tener máximo 100 caracteres",
+      "any.required": "Password es obligatorio",
+      "string.empty": "Password no puede estar vacío",
+    }),
+  })
+  .options({ abortEarly: false });
 
-const esquemaEditarPerfil = joi.object({
-  name: joi.string().min(2).max(50),
-  password: joi.string().min(6).max(100),
-  avatar: joi.allow("")
-}).options({ abortEarly: false });
+const esquemaEditarPerfil = joi
+  .object({
+    name: joi.string().min(2).max(50).messages({
+      "string.base": "Nombre debe ser de tipo texto",
+      "string.min": "Nombre debe tener mínimo 2 caracteres",
+      "string.max": "Nombre debe tener máximo 50 caracteres",
+      "string.empty": "Nombre no puede estar vacío",
+    }),
+    password: joi.string().min(6).max(100).messages({
+      "string.base": "Password debe ser de tipo texto",
+      "string.min": "Password debe tener mínimo 6 caracteres",
+      "string.max": "Password debe tener máximo 20 caracteres",
+    }),
+    avatar: joi.allow(""),
+  })
+  .options({ abortEarly: false });
 
 module.exports = { esquemaRegistro, esquemaLogin, esquemaEditarPerfil };
